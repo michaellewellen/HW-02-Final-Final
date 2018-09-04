ba9c151e4dd31c7fdb9c91f5a6ca92e7a534da6f
Michael Lewellen <michael.lewellen@snow.edu>
2018-09-04 11:33:56 -0600

    Final Final

--- HW-02-Final/MainWindow.xaml.cs
+++ HW-02-Final/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HW_02_Final
+namespace WpfApp3
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,30 @@ namespace HW_02_Final
         public MainWindow()
         {
             InitializeComponent();
+            Monthly.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double interest;
+            double principal;
+            double term;
+            double down;
+            Double.TryParse(Principal.Text, out principal);
+            Double.TryParse(Interest.Text, out interest);
+            interest /= 1200;
+
+            Double.TryParse(Term.Text, out term);
+            term *= 12;
+            Double.TryParse(Down.Text, out down);
+            double temp = (principal - down);
+            double expo = Math.Pow((1 + interest), term);
+            double d = (expo - 1) / (interest * expo);
+            double amount = temp / d;
+            amount = Math.Round(amount, 2);
+            string value = amount.ToString();
+            value = "$" + value;
+            Monthly.Text = (value);
         }
     }
 }
